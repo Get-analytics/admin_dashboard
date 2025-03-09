e26d8ffbf03686d7ce8506355df649b4528f09f4
Get-analytics <team20tech@gmail.com>
2025-03-09 23:44:15 +0530

    Update Linkpage.js
    file upload url updated

--- src/pages/Linkpage.js
+++ src/pages/Linkpage.js
@@ -220,7 +220,7 @@ const Linkpage = () => {
         payload = { shortId: randomId, originalUrl: url, uuid: userInfo.uuid };
         headers["Content-Type"] = "application/json";
       } else if (type === "file") {
-        apiEndpoint = "http://localhost:5000/api/v1/fileupload";
+        apiEndpoint = "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/fileupload";
         payload = new FormData();
         payload.append("shortId", randomId);
         payload.append("file", file);

425539f844659c50ec1616c2fa663b9f7e659e51
Get-analytics <team20tech@gmail.com>
2025-03-11 16:05:44 +0530

    Update Metrics.jsx
    weblink category addedd

--- src/pages/components/Metrics.jsx
+++ src/pages/components/Metrics.jsx
@@ -69,6 +69,7 @@ export default function Metrics({ setActiveMatrix, activeMatrix }) {
           const apiEndpoints = {
             pdf: "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/pdf/analytics",
             weblink: "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/web/analytics",
+            web: "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/web/analytics",
             video: "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/video/analytics",
             docx: "https://admin-dashboard-backend-gqqz.onrender.com/api/v1/docx/analytics",
           };
